0db8acd10795bf64f1063d50949542d28baf880b
AmyStark25 <86607872+AmyStark25@users.noreply.github.com>
2021-07-07 18:20:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function ChangeImage()
+{document.getElementById("img1"). 
+src = "images.jpg";}
+
+function ChangeImage2()
+{document.getElementById("img2"). 
+src = "download-8.jpg";}
+
+function ChangeImage3()
+{document.getElementById("img3"). 
+src = "download-2.jpg";}
+
+function ChangeImage4()
+{document.getElementById("img4"). 
+src = "download-5.jpg";}
+
+
+function ChangeImage5()
+{document.getElementById("img5"). 
+src = "download-7.jpg";}
